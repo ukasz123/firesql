35243c81d1eea64fbce4eaeb403d959b4664e326
Łukasz Huculak <ukasz123@gmail.com>
2024-10-14 19:15:32 +0200

    Add basic editor

--- firesql_cli/src/app/mod.rs
+++ firesql_cli/src/app/mod.rs
@@ -1,47 +1,16 @@
+pub(crate) mod app;
+
 use std::io;
 
-use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
-    layout::{Constraint, Layout},
-    style::{Style, Stylize},
-    text::Line,
-    widgets::{Block, Paragraph},
-};
-use tui_textarea::TextArea;
-
-pub(crate) fn run(
+pub(crate) fn run_app(
+    mut app: app::App,
     mut terminal: ratatui::Terminal<ratatui::prelude::CrosstermBackend<std::io::Stdout>>,
 ) -> io::Result<()> {
-    let mut sql_textarea = TextArea::default();
-    sql_textarea.set_block(Block::bordered().title("Enter SQL:"));
     loop {
-        terminal.draw(|frame| {
-            let greeting =
-                Line::from("Welcome to FireSQL client! (press 'Esc' to quit)").light_red();
-
-            let [title_area, main_area] =
-                Layout::vertical([Constraint::Length(1), Constraint::Min(1)]).areas(frame.area());
-
-            let [left_pane, right_pane] =
-                Layout::horizontal([Constraint::Percentage(50), Constraint::Fill(1)])
-                    .areas(main_area);
-
-            frame.render_widget(greeting, title_area);
-            frame.render_widget(&sql_textarea, left_pane);
-            frame.render_widget(
-                Block::bordered()
-                    .border_style(Style::new().dark_gray())
-                    .title("Firebase query"),
-                right_pane,
-            );
-        })?;
-
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Esc {
-                return Ok(());
-            } else {
-                sql_textarea.input(key);
-            }
-        }
+        terminal.draw(|frame| frame.render_widget(&app, frame.area()))?;
+        match app.handle_events()? {
+            app::EventResult::Quit => return Ok(()),
+            app::EventResult::Continue => {}
+        };
     }
 }
--- firesql_cli/src/main.rs
+++ firesql_cli/src/main.rs
@@ -6,7 +6,9 @@ fn main() -> io::Result<()> {
     println!("Hello, world!");
     let mut terminal = ratatui::init();
     terminal.clear()?;
-    app::run(terminal)?;
+    let a = app::app::App::new();
+    // app::run(terminal)?;
+    app::run_app(a, terminal)?;
     ratatui::restore();
     Ok(())
 }
